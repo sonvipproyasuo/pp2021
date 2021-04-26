77121af746fb39b2c606d23fd5b258278a1bac1e
sonvipproyasuo <81276689+sonvipproyasuo@users.noreply.github.com>
2021-04-26 12:55:19 +0700

    Update input.py

--- pw4/input.py
+++ pw4/input.py
@@ -5,11 +5,19 @@ class information(Student):
         self.dob = dob
         self.studentlist = []
 
-    def showinfo(self):
-        print("Name:" + self.name)
-        print("Id:" + self.id)
-        print("Dob:" + self.dob)
-
     def inputStudent(self, name, id, dob):
         student = input(name, id, dob)
         self.studentlist.append(student)
+
+class Course(Student):
+    def __init__ (self,name, coursename, courseid, coursescore):
+        super().__init__(name)
+        self.coursename = coursename
+        self.courseid = courseid
+        self.coursescore = float(coursescore)
+        self.passcore = 10
+        self.courselist = []
+        
+    def inputCourse(self, coursename, courseid, coursescore):
+        course = input(coursename,courseid, coursescore)
+        self.courselist.append(course)
